e89ea7b5449d73fbdcdf2523a3faa1f2e782f7b5
Patrick Todd <patrick.a.todd@gmail.com>
2016-12-12 16:40:11 -0500

    dictionaries

---
+++ dictionaries/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inventory = new Dictionary<string, double>();
+
+            inventory.Add("figs", 56);
+            inventory.Add("apples", 23);
+            inventory.Add("bananas", 10);
+
+            var keys = inventory.Keys;
+            Console.WriteLine(keys);
+            Console.WriteLine("number of items: " + keys.Count);
+
+            foreach (var key in keys)
+            {
+                Console.WriteLine(key + ": " + inventory[key]);
+            }
+
+            string[] keysArray = keys.ToArray();
+            Array.Sort(keysArray);
+
+            foreach (var key in keysArray)
+            {
+                Console.WriteLine(key + ": " + inventory[key]);
+            }
+        }
+    }
+}
